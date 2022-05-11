d95e011e7b76013bd375ee01eef96ca9789750d5
daniele.carta <daniele.carta@illimity.com>
2022-05-11 21:53:13 +0200

    feat: add vitejs config

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <div>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -16,7 +18,15 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+        </a> & <a
+          className="App-link"
+          href="https://vitejs.dev/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn Vite
         </a>
+        </div>
       </header>
     </div>
   );
